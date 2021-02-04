d694d00b5f0c010746f4bbb1e5edb3371d472778
Erika Yi <68488125+akire2iy@users.noreply.github.com>
2021-02-03 21:45:44 -0500

    final work

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ mongo = PyMongo(app)
 @app.route("/")
 def home():
     mars = mongo.db.mars.find_one()
-    return render_template("index.html", mars=mars)
+    return render_template("index.html")
 
 @app.route("/scrape")
 def scrape():
